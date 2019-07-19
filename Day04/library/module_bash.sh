0880ef26cf70b56d3ff28cf224799508ed6ad668
Hleb Kanonik <50049166+Cyberglamdring@users.noreply.github.com>
2019-07-19 19:41:13 +0300

    Add files via upload

---
+++ Day04/library/module_bash.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+#!/usr/bin/python
+
+DOCUMENTATION = '''
+---
+module: hkanonik_status_check
+short_description: hello
+description: • Process is running as expected (by name, under user)• Port is handled by proper process, in listening mode• Web content of given url contains given regexp string• Web server information (curl -IL url) contains given regexp strin
+author: Hleb Kanonik
+'''
+
+EXAMPLES = '''
+- name: using module
+  hkanonik_status_check:
+    name: start
+    state: present
+'''
+
+cat << EOF
+{
+	"hostname": "$(hostname)",
+	"process": "$(pidof httpd)",
+	"netstat": "$(netstat -lntup | grep httpd)"
+}
+EOF
+
+
+
+#	"process": "systemctl status httpd"
+#	"netstat": "netstat -lntup | grep httpd"
+#	"httpd" : "wget -qO- http://178.65.0.5/ | grep -Eoi httpd"
+#	"curl": "curl -IL http://178.65.0.5/"
+# "httpd" : "$(curl -IL 'http://178.65.0.5/')"
